feb7105486354791d0ffc33dacdc85f45f032a8b
Jonathan Black <Jonathan@Galaxite.net>
2020-10-11 03:58:54 +0100

    t

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.js"></script>
+    <!--<script type="text/javascript" sec="js/"></script>-->
+</head>
+<body>
+  <div data-role="page">
+  <div id="header" data-role="header">
+    <h1>Header</h1>
+    </div>
+    <div id="content" data-role="content">
+      <p>This is content</p>
+    </div>
+    <div id="footer" data-role="footer">
+      <h1>This is a footer</h1>
+    </div>
+
+  </div>
+  </div>
+
+</body>
+</html>
